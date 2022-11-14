be0320e0eea87785d773d494646e88d9bf3f91c9
Metehan Metinoğlu <mete3777@gmail.com>
2022-11-15 02:37:31 +0300

    First_Commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace try_catch;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Bir sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı: "+ sayi);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Hata: "+ ex.Message.ToString());
+        }
+        finally
+        {
+            Console.WriteLine("İşlem tamamlandı..!");
+        }
+
+        try
+        {
+            //int s=int.Parse(null);
+            //int a = int.Parse("test");
+            int s1=int.Parse("2222222222222222");
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine("Boş değer girdiniz");
+            Console.WriteLine(ex);
+        }
+        catch(FormatException ex)
+        {
+            Console.WriteLine("Veri tipi uygun değil");
+            Console.WriteLine(ex);
+        }
+        catch(OverflowException ex)
+        {
+            Console.WriteLine("Veri sınır dışında");
+            Console.WriteLine(ex);
+        }
+        finally
+        {
+            Console.WriteLine("İşlem tamam");
+        }
+    }
+}
